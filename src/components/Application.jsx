61a48af08a5eb719b98da19a84f861c545c69eb5
Steve Kinney <hello@stevekinney.net>
2018-12-12 13:49:44 -0600

    Wire up Cloud Firestore
    Implement getting all posts and adding a new post.

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,44 +1,32 @@
 import React, { Component } from 'react';
 
 import Posts from './Posts';
+import { firestore } from '../firebase';
 
 class Application extends Component {
   state = {
-    posts: [
-      {
-        id: '1',
-        title: 'A Very Hot Take',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '123',
-          displayName: 'Bill Murray',
-          email: 'billmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/300/300',
-        },
-        stars: 1,
-        comments: 47,
-      },
-      {
-        id: '2',
-        title: 'The Sauciest of Opinions',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '456',
-          displayName: 'Mill Burray',
-          email: 'notbillmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/400/400',
-        },
-        stars: 3,
-        comments: 0,
-      },
-    ],
+    posts: [],
   };
 
-  handleCreate = post => {
+  componentDidMount = async () => {
+    const snapshot = await firestore.collection('posts').get();
+
+    const posts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    this.setState({ posts });
+  };
+
+  handleCreate = async (post) => {
+    const documentReference = await firestore.collection('posts').add(post);
+    const data = await documentReference.get();
+
+    const newPost = {
+      id: documentReference.id,
+      ...data
+    }
+
     const { posts } = this.state;
-    this.setState({ posts: [post, ...posts] });
+    this.setState({ posts: [newPost, ...posts] });
   };
 
   render() {
