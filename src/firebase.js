57e690f77679d285321f2fa5376ff18b261e5e81
Steve Kinney <hello@stevekinney.net>
2018-12-13 12:03:40 -0600

    Remove my keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-  apiKey: 'AIzaSyCcCh4dS3sm_VYit66Edm1Y8-vQaLqzwJk',
-  authDomain: 'think-piece-live.firebaseapp.com',
-  databaseURL: 'https://think-piece-live.firebaseio.com',
-  projectId: 'think-piece-live',
-  storageBucket: 'think-piece-live.appspot.com',
-  messagingSenderId: '293948081167',
-};
+import config from './config.json';
 
 firebase.initializeApp(config);
 
