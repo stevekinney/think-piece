268ddbdf2500141b7ec2ab949ace6ad2ae77a900
Steve Kinney <hello@stevekinney.net>
2018-12-13 14:33:19 -0600

    React Context API and Hooks

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Post from './Post';
 import AddPost from './AddPost';
+import { PostsContext } from '../providers/PostsProvider';
 
-const Posts = ({ posts }) => {
+const Posts = () => {
+  const posts = useContext(PostsContext);
+
   return (
     <section className="Posts">
       <AddPost />
-      {posts.map(post => (
-        <Post {...post} key={post.id} />
-      ))}
+      {posts.map(post => <Post {...post} key={post.id} />) }
     </section>
   );
 };
