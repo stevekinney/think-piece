51d41e121c831e2735608d4d4059cb718df35e22
Steve Kinney <hello@stevekinney.net>
2018-12-13 11:59:12 -0600

    Exercise: Authentication

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,6 +6,7 @@ import { firestore } from '../firebase';
 const Post = ({ id, title, content, user, createdAt, stars, comments }) => {
   const postRef =  firestore.doc(`posts/${id}`);
   const remove = () => postRef.delete();
+  const star = () => postRef.update({ stars: stars + 1 });
 
   return (
     <article className="Post">
@@ -28,10 +29,10 @@ const Post = ({ id, title, content, user, createdAt, stars, comments }) => {
             {comments}
           </p>
           <p>Posted by {user.displayName}</p>
-          <p>{moment(createdAt).calendar()}</p>
+          <p>{moment(createdAt.toDate()).calendar()}</p>
         </div>
         <div>
-          <button className="star">Star</button>
+          <button className="star" onClick={star}>Star</button>
           <button className="delete" onClick={remove}>Delete</button>
         </div>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
   apiKey: 'AIzaSyCcCh4dS3sm_VYit66Edm1Y8-vQaLqzwJk',
@@ -13,6 +14,10 @@ const config = {
 firebase.initializeApp(config);
 
 export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 firestore.settings({ timestampsInSnapshots: true });
 
