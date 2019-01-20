7a03e1aa4ff1167a1fb36cf59624fa2eaafc5d8a
cacheUp <bradb26@gmail.com>
2019-01-20 22:21:17 +0300

    fixed the typo in the import

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,16 +1,12 @@
-import React from 'React';
+import React from "react";
 
-import CurrentUser from './CurrentUser';
-import SignInAndSignUp from './SignInAndSignUp';
+import CurrentUser from "./CurrentUser";
+import SignInAndSignUp from "./SignInAndSignUp";
 
 const Authentication = ({ user, loading }) => {
   if (loading) return null;
 
-  return (
-    <div>
-      { user ? <CurrentUser /> : <SignInAndSignUp /> }
-    </div>
-  )
+  return <div>{user ? <CurrentUser /> : <SignInAndSignUp />}</div>;
 };
 
 export default Authentication;
