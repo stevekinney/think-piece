f0ff51289e4c549f44ec7d6196a98e76f960182e
Steve Kinney <hello@stevekinney.net>
2019-02-28 14:02:29 -0700

    Update Authentication.jsx

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import CurrentUser from "./CurrentUser";
-import SignInAndSignUp from "./SignInAndSignUp";
+import CurrentUser from './CurrentUser';
+import SignInAndSignUp from './SignInAndSignUp';
 
 const Authentication = ({ user, loading }) => {
   if (loading) return null;
